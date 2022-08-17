a5b4fc21db58f523ebba42780bf40ae3c4e22148
Yi Jiahe <yi.jiahe00@gmail.com>
2022-08-17 17:44:06 +0800

    fix missing semicolons

--- src/cli.h
+++ src/cli.h
@@ -1,9 +1,9 @@
 #ifndef CLI_H
 #define CLI_H
 
-constexpr int kLineBufSize = 128   //Maximum input string length
-constexpr int kArgBufSize = 64     //Maximum argument string length
-constexpr int kMaxNumArgs = 8      //Maximum number of arguments
+constexpr int kLineBufSize = 128;   //Maximum input string length
+constexpr int kArgBufSize = 64;     //Maximum argument string length
+constexpr int kMaxNumArgs = 8;      //Maximum number of arguments
 
 extern char line[kLineBufSize];
 extern char args[kMaxNumArgs][kArgBufSize];
